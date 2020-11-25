fe43052fc371260a5e77d3c1452ecb26d6bd140b
David Malek <maldav31@ecolecatholique.ca>
2020-11-25 15:38:00 +0000

    Added .gitignore file

---
+++ formatif1/hello_David.py
@@ -0,0 +1,8 @@
+#Tâche formative Hello World de David Malek en 3U
+#Voici le code pour transmettre mon message
+print("Hello")
+print("world")
+print("from")
+print("Canada!")
+language = "python3"
+run = "python formatif1/hello_David.py"
