c3302395df23c92ac984762ec024d18b769d4b6b
David Malek <maldav31@ecolecatholique.ca>
2020-12-06 17:57:22 +0000

    Finalised everything

--- Sommatifs/PIC.py
+++ Sommatifs/PIC.py
@@ -2,7 +2,7 @@
 #Par David Malek, maldav31@ecolecatholique.ca
 # 2020/12/06
 
-# 1. Présentation entre utilisateur et console
+### 1. Présentation entre utilisateur et console
 
 print("")
 print("Insérer votre nom d'utilisateur:")
@@ -11,21 +11,21 @@ print("")
 print("Salut",name,", je me nomme MathMaj et je suis un magicien!")
 print("Aimes-tu les mathématiques? (répondre par oui ou non)")
 
-#Réponse oui ou non (si « oui », il continue. Si « non », ne peut pas continuer)
+## 1.1 Réponse oui ou non (si « oui », il continue. Si « non », ne peut pas continuer)
 
-réponse_1= input().lower()
+réponse_1= input().lower() # oui/non rest minuscule
 if réponse_1 == "oui":
   print("Excellent! Je vais essayer de vous tromper avec un problème à résoudre!")
-  estVrai= True
+  estVrai= True #oui est vrai donc l'utilisateur continue avec le programme dans cette condition
 elif réponse_1== "non":
   print("C'est dommage, on aurait pu s'amuser ensemble avec les maths!")
-  estVrai= False
+  estVrai= False #Le programme va arrêter l'utilisateur de continuer car c'est un programmme pour ceux passionés en maths.
 else:
   print("Je n'ai pas compris votre réponse. Répondez simplement par oui ou non après avoir cliqué sur Run encore une fois.")
-  estVrai= False
+  estVrai= False #Réponse non valide donc l'utilisateur ne peux pas continuer
 
-# Devinette 1
-if estVrai:
+### 2. DEVINETTE 1
+if estVrai: # si oui, utilisateur continue dans le programme
   print("")
   print("DEVINETTE 1:")
   print("Alors commençons avec la première devinette! Soit le nombre entier x:")
@@ -35,22 +35,22 @@ if estVrai:
   print("Aussi, mes chiffres sont chacuns trouvés une fois seulement dans le nombre. Quel est ce nombre?")
   print("Entrer votre réponse ci-dessous:")
 
-# Code de la réponse de devinette 1
+## 2.1 Code de la réponse de devinette 1
   choix= input()
   if choix == "126":
-    estVrai= True
+    estVrai= True #bonne réponse donc: vrai
     print("Bravo! Vous avez la bonne réponse!")
-  elif choix != "126":
+  elif choix != "126": #tout les réponses non égales sont fausses
     print("Votre réponse n'est pas valide, vous avez une autre chance")
     chance= input()
     if chance== "126":
-      estVrai= True
+      estVrai= True # nouvelle chance pour avoir bonne réponse
       print("Bravo, vous avez eu la bonne réponse cette fois.")
     elif chance!= "126":
-      estVrai= False
+      estVrai= False # même chose que ligne 43 mais utilisateur ne peut pas continuer. Doit recommencer le programme
       print("Mauvaise réponse, vous pouvez essayer encore en cliquant sur Run.")
 
-# DEVINETTE 2
+### 3. DEVINETTE 2
 if estVrai:
   print("")
   print("DEVINETTE 2:")
@@ -61,7 +61,8 @@ if estVrai:
   print("qui est plus grand que mon nombre initial par 27.")
   print("Quel est mon nombre final?")
 
-# Code de la réponse de devinette 2
+## 3.1 Code de la réponse de devinette 2 
+# (mêmes instructions que devinette 1) section 2.1 ligne 38
   final= input()
   if final== "63":
     print("Bravo, vous avez la bonne réponse!")
@@ -75,16 +76,17 @@ if estVrai:
       print("Mauvaise réponse, revenez une autre fois.")
       estVrai= False
 
-# Rétroaction
 
-if estVrai:
+### 4. Rétroaction
+
+if estVrai: # si l'utilsateur aime les maths et arrive à ce point après avoir eu les bonnes réponses des 2 devinettes, il suggère son opinion comme la console le demande.
   print("")
   print("")
   print("As-tu aimé les deux devinettes? (répondre par oui ou non)")
-  answer= input().lower()
+  answer= input().lower() #comme dans la section de présentation entre utilisateur et console: ligne 5
   if answer== "oui":
     print("Je suis content que tu les aimes. Merci beaucoup pour avoir participé!")
   elif answer== "non":
     print("AH! C'est dommage! Je m'amusait bien avec toi! Reviens demain pour de nouvelles devinettes, possiblement dans un autre domaine!")
   else:
-    print("Je n'ai pas compris donc je vais présumer que c'est un oui. Merci pour avoir participé!")
+    print("Je n'ai pas compris donc je vais présumer que c'est un oui. Merci pour avoir participé!") #blague pour dire que les réponses incompréhensibles vont êtres présumés comme étant «oui».
