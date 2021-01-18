45bf8d00a6a89454dde0e03940a60ed710273de2
David Malek <maldav31@ecolecatholique.ca>
2021-01-18 19:32:21 +0000

    Programme base.py

--- school/Sommatifs/BPZ/base.py
+++ school/Sommatifs/BPZ/base.py
@@ -1,40 +1,54 @@
 #Tâche: Programme interactif à la console en python
 #Par David Malek, maldav31@ecolecatholique.ca
-# 2021/01/15
-
+#2021/01/15
 import pgzrun
 
-WIDTH= 700
-HEIGHT= 500
+WIDTH= 400
+HEIGHT= 300
+keep_play= True
 
-#ballon basket
-ball= Actor("bball.png")
-ball.pos= (0, 100)
+#Les instructions pour marquer un but
+instructions=("Marquer un but en utilisant les clés \n W,A,S,D pour déplacer le ballon.")
+
+#Sprite ballon basket
+ball= Actor("b-ball.png")
+ball.pos= (20,275)
 spin= 3
 
-#Fillet basket
+#Sprite fillet basket
 net= Actor("basketnet.png")
-net.pos= (700,100)
+net.pos= (375,100)
 
 #Affichage graphique 
 def draw():
   screen.fill("white")
   ball.draw()
   net.draw()
+  screen.draw.text(instructions, (0,0), color="black", fontsize=23)
 
-#Rotation du ballon
+#Rotation du ballon et marquer un but
 def update():
-  ball.angle += spin
+  ball.angle -= spin
+  if (ball.colliderect(net)):
+    keep_play= False
 
 #Mouvement du ballon
 def on_key_down(key):
-  if key== keys.w:
-    ball.y -= 100
-  if key== keys.d:
-    ball.x += 50 
-  if key== keys.s:
-    ball.y += 100
-  if key== keys.a:
-    ball.x -= 50
-  
+  if key== keys.W:
+    ball.y -= 10
+  elif key== keys.D:
+    ball.x += 10 
+  elif key== keys.S:
+    ball.y += 10
+  elif key== keys.A:
+    ball.x -= 10
+
+""" #option à essayer (je viens de l'écrire)
+while keep_play:
+  draw()
+  update()
+  on_key_down(key)
+  if (ball.colliderect(net)):
+    keep_play= False
+""""
 pgzrun.go()
