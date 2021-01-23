0e8698744582a40e22c579c85b9c8da9c3ab1de4
David Malek <maldav31@ecolecatholique.ca>
2021-01-23 21:10:16 +0000

    Updated game

--- school/tests/projet/game.py
+++ school/tests/projet/game.py
@@ -1,4 +1,4 @@
-#Tâche: Programme base pygame en python
+#Tâche: Programme pour projet pgzero en python
 #Par David Malek, maldav31@ecolecatholique.ca
 #2021/01/18
 import pgzrun
@@ -6,48 +6,68 @@ import random
 
 WIDTH=500
 HEIGHT=300
+dead= False 
+
+#Sprite space
 space= Actor("space.png")
 
 
+# Sprite jetpack
 jetpack= Actor("jetpack.png")
-jetpack.pos= (50, 250)
+jetpack.pos= (50, 250) #Position initial de jetpack
 
+
+#Sprite rocket
 rocket= Actor("rocket.png")
+rocket.x= WIDTH
+rocket.y= 150 
+
 
 score= 0 #Score initial au commencement
-rocket.speed= -1# Vitesse initiale des fusées
-dead= False # Quand True, le personnage est entré en collision avec fusée
-
-def reset_faster():
-    rocket.speed += 1
-    rocket.x = (WIDTH)
-    rocket.y = random.randint(0, HEIGHT, rocket.height)
+speed= 2 # Vitesse initiale des fusées
 
 
 def draw():
   space.draw()
   screen.draw.text("Score: " + str(score), midtop = (WIDTH / 2, 5), fontsize = 30)
-  if (not dead):
+  if dead:
     jetpack.draw()
     rocket.draw()
-
   else:
     screen.draw.text("YOU DIED", center= (WIDTH/2, HEIGHT/2), fontsize = 50)
 
 
-
 def update():
-  global score, dead
+  global speed, score, dead
+
+  #Controller bonhomme veticalement
   if keyboard.w:
-    jetpack.y -= 5 
+    jetpack.y -= 5
   if keyboard.s:
-    jetpack.y += 5
+    jetpack.y += 5 
+  # Limiter la région de déplacement verticale du bonhomme
+  if jetpack.y < 0 or jetpack.y > HEIGHT:
+    jetpack.y == 0, HEIGHT
 
-  if (rocket.x <= 0):
-    score += 1    
-    rocket.x += rocket.speed
 
-  if jetpack.colliderect(rocket):
+  if (rocket.x>0):
+    rocket.x -= speed # Déplacement horizontale de la fusée
+  else:
+    speed += 1 # Quand déplacement est <= 0, speed augmente de 1 pixel pour le prochain update
+    if speed >= 15: 
+      speed = 13 # quand speed >= 15, garder une « vitesse constante » de 13 pixels par update
+      # augmenter « vitesse de déplacement » du bonhomme
+      if keyboard.w:
+        jetpack.y -= 8
+      if keyboard.s:
+        jetpack.y += 8 
+    
+    score += 1 # score augmente quand une fusée sort de la gauche de l'écran
+    rocket.x = WIDTH #repositionnement de la fusée vers la droite
+    rocket.y = random.randint(0, HEIGHT) #Repositionnement au hasard de la fusée (verticalement)
+
+  # Si bonhomme entre en contact avec fusée, bonhomme meurt
+  if (jetpack.colliderect(rocket)):
     dead= True
 
 
