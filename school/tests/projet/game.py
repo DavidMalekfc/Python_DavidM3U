6585a01cca5f7ab6cdb338a532523825321b004e
David Malek <maldav31@ecolecatholique.ca>
2021-01-26 21:09:01 +0000

    Added instructions.md file

--- school/tests/projet/game.py
+++ school/tests/projet/game.py
@@ -4,8 +4,8 @@
 import pgzrun
 import random
 
-WIDTH=500
-HEIGHT=300
+WIDTH=500  #Longueur de la base de la fenêtre
+HEIGHT=300 #Hauteur de la 
 dead= False 
 
 #Sprite space
@@ -26,7 +26,7 @@ rocket.y= 150
 score= 0 #Score initial au commencement
 speed= 2 # Vitesse initiale des fusées
 
-
+print(rocket.height, jetpack.height)
 
 def draw():
   space.draw()
@@ -40,35 +40,39 @@ def draw():
 
 def update():
   global speed, score, dead
-  #Controller bonhomme veticalement
+
+  #Condition quand bonhomme est vivant
   if not dead: 
+
+    #Déplacer bonhomme veticalement
     if keyboard.w:
       jetpack.y -= 5
     if keyboard.s:
       jetpack.y += 5 
 
-    if jetpack.y <= 0: 
-      jetpack.y= 0
+    # Limiter région de déplacement du bonhomme à la hauteur de la fenêtre
+    # Condition: En enlevant de l'extrémité une certaine longueure (la hauteur de jetpack/2) pour que bohomme ne soit pas demi caché. 
+    #SINON, on peut mettre une limite de <=0 et >=HEIGHT 
+    if jetpack.y <= (0 + jetpack.height/2): 
+      jetpack.y= (0 + jetpack.height/2)
 
-    elif jetpack.y >= HEIGHT:
-      jetpack.y= HEIGHT
+    elif jetpack.y >= (HEIGHT - jetpack.height/2):
+      jetpack.y= (HEIGHT - jetpack.height/2)
 
-    if (rocket.x>0):
-      rocket.x -= speed # Déplacement horizontale de la fusée
-  
+    # Code qui gère les déplacements horizotales des fusées quand position "x" de la fusée > 0
+    if rocket.x>0:
+      rocket.x -= speed # Déplacement horizontale de la fusée (droite vers la gauche)
+
+    # Quand position "x" de fusée < 0
     else:
       speed += 1 # Quand déplacement est <= 0, speed augmente de 1 pixel pour le prochain update
-      if speed >= 12: 
-        speed = 12 # quand speed >= 15, garder une « vitesse constante » de 13 pixels par update
-        # augmenter « vitesse de déplacement » du bonhomme
-        if keyboard.w:
-          jetpack.y -= 15
-        if keyboard.s:
-          jetpack.y += 15
-        
-      score += 1 # score augmente quand une fusée sort de la gauche de l'écran
+      if speed >= 10: 
+        speed = 10 # quand speed >= 10, garder une « vitesse constante » de 10 pixels par update
+
+      score += 1 # score augmente quand une fusée sort de la gauche de l'écran x <= 0
       rocket.x = WIDTH #repositionnement de la fusée vers la droite
-      rocket.y = random.randint(0, HEIGHT) #Repositionnement au hasard de la fusée (verticalement)
+      rocket.y = random.randint((0 + jetpack.height/2), (HEIGHT-jetpack.height/2)) #Repositionnement au hasard de la fusée (verticalement)... 
+      #La région est limitée à la fenêtre, mais en limitant davantage une longueur (la hauteur de jetpack/2) à partir de 0 et HEIGHT.
 
   # Si bonhomme entre en contact avec fusée, bonhomme meurt
   if jetpack.collidepoint(rocket.midleft) or jetpack.collidepoint(rocket.midtop) or jetpack.collidepoint(rocket.midbottom)  :
---
+++ school/tests/projet/instructions.md
@@ -0,0 +1,16 @@
+Les instructions pour le jeu SPACE DASH:
+
+Un bonhomme voyage dans le comos sur son jet-pack mais soudainement, il se fait attaqué par des fusées! Il a besoin de votre aide pour lui guider et lui aider à ne pas être tué. 
+
+Comment faire: Les fusées apparaîtrons sur l'écran et voyageront de la droite vers la gauche. C'est alors qu'avec les clés W et S vous pouvez guider le bonhomme verticalement. 
+
+    W: pour se déplacer vers le haut
+    S: pour se déplacer vers le bas
+  (L'espace de déplacement est limité)
+
+
+DANGER: Attention au fusées! Si vous entrez en contact avec une, le bonhomme mourra. 
+
+Essayez de vivre le plus longtemps possible. Le score augmente selon nombre de fusées esqivés (1 fusée évitée = 1 point).
+
+Pour commencer le voyage, faire RUN.
